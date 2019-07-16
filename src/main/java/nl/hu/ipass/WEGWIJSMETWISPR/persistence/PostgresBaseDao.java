870c68f384d0295c38be67fe2dd7d2a3a4346d9b
VincentHUict <vincent.sels@student.hu.nl>
2019-07-16 14:04:19 +0200

    fixed main and postgresbasedao

--- src/main/java/nl/hu/ipass/WEGWIJSMETWISPR/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/ipass/WEGWIJSMETWISPR/persistence/PostgresBaseDao.java
@@ -1,52 +1,21 @@
 package nl.hu.ipass.WEGWIJSMETWISPR.persistence;
 
-import java.net.URI;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-//import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class PostgresBaseDao {
-    private static DataSource connectionPool;
-    
-    public PostgresBaseDao() {
-    	if (connectionPool == null) {
-    		try {
-    			final String DATABASE_URL = System.getenv("DATABASE_URL");
-    			
-//    			if (DATABASE_URL != null) { //heroku
-//    				URI dbUri = new URI(DATABASE_URL);
-//    				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-//    				BasicDataSource bds = new BasicDataSource();
-//    				
-//    				connectionPool = bds;
-//    			} else { //lokaal
-//    				InitialContext ic = new InitialContext();
-//    				connectionPool = (DataSource)ic.lookup("java:comp/env/jdbc/PostgresDS");
-//    			}
-    		} catch (Exception exc) {
-    			throw new RuntimeException(exc);
-    		}
-    	}
-    }
-    
-    public Connection getConnection() {
-    	Connection result = null;
-    	
-    	try {
-    		InitialContext ic = new InitialContext();
-    		DataSource ds = (DataSource)ic.lookup("java:comp/env/jdbc/PostgresDS");
-    		result = ds.getConnection();
-    	} catch (Exception exc) {
-    		throw new RuntimeException(exc);
-    	}
-    	return result;
-    }
-   
-	public void closeConnection() throws SQLException {
-		Connection myConn = getConnection();
-		myConn.close();
+	protected final Connection getConnection() {
+		Connection result = null;
+
+		try {
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+
+			result = ds.getConnection();
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		return result;
 	}
 }
--- src/main/java/nl/hu/ipass/WEGWIJSMETWISPR/servlet/Main.java
+++ src/main/java/nl/hu/ipass/WEGWIJSMETWISPR/servlet/Main.java
@@ -1,8 +1,21 @@
 package nl.hu.ipass.WEGWIJSMETWISPR.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class Main extends HttpServlet
-{
-
+@WebServlet(urlPatterns = "/IpassProject.do")
+public class Main extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String naam = req.getParameter(getServletName());
+		String ip = req.getRemoteAddr();
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		out.println(naam + ip + "jemoeder");
+	}
 }
